050d436863d62e7182f5e1cd48f520e066e9967a
itggot-rikard-sjostedt <rikard.sjostedt@itggot.se>
2017-05-09 15:15:13 +0200

    info and first flight + nav and opening done for all pages
    should make a proper fix for iframe scaling

--- site/js/main.js
+++ site/js/main.js
@@ -11,9 +11,22 @@ $(window).on('scroll', function ()
     if ($(window).scrollTop() >= $('.opening').height()-1)
     {
         $('.nav_desktop').addClass('stick');
+        $('.navfix').removeClass('hidden');
     }
     else
     {
         $('.nav_desktop').removeClass('stick');
+        $('.navfix').addClass('hidden');
     }
 });
+
+
+function openNav() 
+{
+    document.getElementsByClassName("sidenav")[0].style.width = "100%";
+}
+
+function closeNav() 
+{
+    document.getElementsByClassName("sidenav")[0].style.width = "0";
+}
